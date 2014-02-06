11df76be5ea9779a48cc61a226649673d192afe8
Wesley Hoffman <Wesley@Wesley-Hoffmans-MacBook-Pro.local>
2014-02-06 13:27:01 -0600

    Added new fields and changed the text color.

--- src/Web-UI-Quaternion.rkt
+++ src/Web-UI-Quaternion.rkt
@@ -6,16 +6,27 @@
   (response/xexpr
    '(html
      (head (title "Quaternion Calcluator"))
-     (body ((style "background-color: BLACK; font-family: frosty; font-size: 12pt; font-color: WHITE"))
+     (body ((style "background-color: BLACK; font-family: frosty; font-size: 12pt; color: WHITE"))
             (center (h1 "Quaternion Calculator"))(hr)
             (form 
                   (nbsp "Enter: ")
-                  (input ((name "constant")(size "2")))
+                  (input ((name "constant1")(size "2")))
                   (nbsp " + ")
-                  (input ((name "i")(size "2")))
+                  (input ((name "i1")(size "2")))
+                  (nbsp "i + ")
+                  (input ((name "j1")(size "2")))
+                  (nbsp "j + ")
+                  (input ((name "k1")(size "2")))
+                  (nbsp "k")(br)
+                  
+                  (nbsp "Enter: ")
+                  (input ((name "constant2")(size "2")))
                   (nbsp " + ")
-                  (input ((name "j")(size "2")))
-                  (nbsp " + ")
-                  (input ((name "k")(size "2"))))))))
+                  (input ((name "i2")(size "2")))
+                  (nbsp "i + ")
+                  (input ((name "j2")(size "2")))
+                  (nbsp "j + ")
+                  (input ((name "k2")(size "2")))
+                  (nbsp "k"))))))
                   
 
