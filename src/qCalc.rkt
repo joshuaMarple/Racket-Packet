eb5df258d03d6f549ccdc04bc5bc4e8316d367bf
Kristopher Goering <kgoering@cycle2.eecs.ku.edu>
2014-02-06 16:14:14 -0600

    More functions available

--- src/qCalc.rkt
+++ src/qCalc.rkt
@@ -1,18 +1,18 @@
 #lang racket
 (provide qCalc)
-(require "QuaternionCalc.rkt")
+(require "arithmetic.rkt")
 
 (define (qCalc op elements)
-  (case (op)
-    [("add") (q+ elements)]
-    [("subtract") (q- elements)]
-    [("multiply") (q* elements)]
-    [("divide") (q/ elements)]
-    [("sin") (qsin elements)]
-    [("cos") (qcos elements)]
-    [("magnitude") (qmag elements)]
-    [("log") (qlog elements)]
-    [("exponent") (qexp elements)]
-    [("e") (qexpt elements)]
-    [("equals") (q= elements)]
+  (case op
+    [(add) (q+ elements)]
+    [(subtract) (q- elements)]
+    [(multiply) (q* elements)]
+    [(divide) (q/ elements)]
+;    [(sin) (qsin elements)]
+;    [(cos) (qcos elements)]
+    [(magnitude) (qmag elements)]
+;    [(log) (qlog elements)]
+    [(exponent) (qexpt elements)]
+;    [(e) (qexp elements)]
+    [(equals) (q= elements)]
     [else "Error"]))
