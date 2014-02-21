6dd854469224d90f462afdffce3f9d1dfdeb346f
Kristopher Goering <kgoering@1005d-5.eecs.ku.edu>
2014-02-21 13:32:32 -0600

    Fixed sin and cos

--- src/trig.rkt
+++ src/trig.rkt
@@ -19,15 +19,15 @@
 
 (define (ve v)
   (cons (cos (qmag v)) (map (lambda (x) (* (sin(qmag v)) x)) (toUnit v))))
-
+          
 (define (qsin q)
-  (q/ (list (q- (list (qexp (q* (list q (append (list 0) (toUnit(rest q)))))) 
-  (qexp (q* (list(list -1 0 0 0) (q* (list q (append (list 0) (toUnit(rest q))))))))
-                      (q* (list (list 2 0 0 0) (append (list 0) (toUnit(rest q))))))))))
-                      
+  (q/ (list (q- (list (qexp (q* (list q (append (list 0)(toUnit(rest q)))))) 
+                (qexp (map - (q* (list q (append (list 0)(toUnit(rest q)))))))))
+            (q* (list '(2 0 0 0 ) (append (list 0)(toUnit(rest q))))))))
+
 (define (qcos q)
-  (q/ (list (q+ (list (qexp (q* (list (append (list 0) (toUnit(rest q))) q))) 
-  (qexp (q* (list (list -1 0 0 0) (q* (list (append (list 0) (toUnit(rest q))) q)))))))
-                      (q* (list (list 2 0 0 0) (append (list 0) (toUnit(rest q))))))))
+  (q/ (list (q+ (list (qexp (q* (list q (append (list 0)(toUnit(rest q)))))) 
+                (qexp (map - (q* (list q (append (list 0)(toUnit(rest q)))))))))
+            (q* (list '(2 0 0 0 ) (append (list 0)(toUnit(rest q))))))))
 ;'(-2.958 -3.81 ...)
 ;'(2.4525 .853 .. . )
