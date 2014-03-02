bb1f69fb9d9921a3385df381511459e355ee6dc8
captainhammer314 <tdfrese@gmail.com>
2014-03-02 13:35:31 -0600

    Update Tests

--- tests/qtest_helper.rkt
+++ tests/qtest_helper.rkt
@@ -3,7 +3,7 @@
 (require rackunit)
 
 (define (qtest_helper result answer)
-  (check-= (list-ref result 0) (list-ref answer 0) 0.01 "real component is off by at least 0.01")
-  (check-= (list-ref result 1) (list-ref answer 1) 0.01 "'i' component is off by at least 0.01")
-  (check-= (list-ref result 2) (list-ref answer 2) 0.01 "'j' component is off by at least 0.01")
-  (check-= (list-ref result 3) (list-ref answer 3) 0.01 "'k' component is off by at least 0.01"))
+  (check-= (list-ref result 0) (list-ref answer 0) 0.1 "real component is off by at least 0.1")
+  (check-= (list-ref result 1) (list-ref answer 1) 0.1 "'i' component is off by at least 0.1")
+  (check-= (list-ref result 2) (list-ref answer 2) 0.1 "'j' component is off by at least 0.1")
+  (check-= (list-ref result 3) (list-ref answer 3) 0.1 "'k' component is off by at least 0.1"))
--- tests/test_e.rkt
+++ tests/test_e.rkt
@@ -5,5 +5,13 @@
 (provide test_e)
 (define (test_e)
   (qtest_helper (qexp '(1 1 1 1)) '(-0.43644 1.549 1.549 1.549))
+  (qtest_helper (qexp '(0 0 0 1)) '(0.5403 0 0 0.84147))
+  (qtest_helper (qexp '(0 0 1 0)) '(0.5403 0 0.84147 0))
+  (qtest_helper (qexp '(0 1 0 0)) '(0.5403 0.84147 0 0))
+  (qtest_helper (qexp '(1 0 0 0)) '(2.7183 0 0 0))
+  (qtest_helper (qexp '(8 -2 3 -1)) '(-2460.2 899.78 -1349.7 449.89))
+  (qtest_helper (qexp '(3 3 -1 -2)) '(-16.577 -9.094 3.0313 6.0627))
+  
+  ;(qtest_helper (qexp '()) '())
   
   (displayln "test e^ complete"))
